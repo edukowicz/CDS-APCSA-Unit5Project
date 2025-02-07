0428ab7a5fdb1e151f531ab22e66c44821889b52
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-02-07 14:23:25 +0000

    message

--- Day 1/MainOne.java
+++ Day 1/MainOne.java
@@ -1,38 +1,62 @@
 // Air Quality Analyzer Starter 1   A+CR
-class Main {
+class MainOne {
 
     // make sure to pass the correct arguments when calling the methods
-    
-    
+ 
+
       public static void main(String[] args) {
         
         //create 1 array of City objects and initialize the array with City objects using the loadValues() method in the City class
+        CityOne[] cities = CityOne.loadValues();
     
-    
+
         //print all the cities and their PM2.5 level
+        for(int i = 0; i < cities.length; i++) {
+          System.out.println(cities[i]);
+        }
     
     
         //call the findMax method and print all the cities (using a loop) with the worst air quality
+        for(int i = 0; i < cities.length; i++) {
+          if(cities[i].getPMValue() == findMax(cities)){
+            System.out.println(cities[i].getName() + " has the worst air quality of " + findMax(cities));
+          }
+        }
     
     
         //call the findMin method and print all the cities (using a loop) with the best air quality
+        for(int i = 0; i < cities.length; i++) {
+          if(cities[i].getPMValue() == findMin(cities)){
+            System.out.println(cities[i].getName() + " has the best air quality of " + findMin(cities));
+          }
+        }
     
-    
+
       } //end of main method
     
     
         // The method below should find the maximum pollution value in the array of City objects instantiated above 
         // The method will need to call the getPMValue method in the City class to retrieve each city's pollution value and return the maxinum pollution value
-        public static int findMax(City[] c) {
-    
-            return -1;
+        public static int findMax(CityOne[] c) {
+          int temp = 0;
+          for(int i = 0; i < c.length; i++) {
+            if(c[temp].getPMValue() < c[i].getPMValue()) {
+              temp = i;
+            }
+          }
+          return c[temp].getPMValue();
         }
     
         // The method below should find the minimum pollution value in the array of City objects instantiated above 
         // The method will need to call the getPMValue method in the City class to retrieve each city's pollution value and return the minimum pollution value
-        public static int findMin(City[] c) {
-    
-            return -1;
+        public static int findMin(CityOne[] c) {
+          int temp = 0;
+          for(int i = 0; i < c.length; i++) {
+            if(c[temp].getPMValue() > c[i].getPMValue()) {
+              temp = i;
+            }
+          }
+          return c[temp].getPMValue();
         }
       
     }
--- Day 2/MainTwo.java
+++ Day 2/MainTwo.java
@@ -1,13 +1,14 @@
 // Air Quality Analyzer 2    A+CR
 import java.util.ArrayList;
 
-class Main {
+class MainTwo {
 
   // make sure to pass the correct arguments when calling the methods
 
 
   public static void main(String[] args) {
     //instantiate 1 ArrayList of City objects and initialize the ArrayList with City objects using the loadValues() method in the City class
+	ArrayList<CityTwo> cities = CityTwo.loadValues();
 
 
 
@@ -15,17 +16,29 @@ class Main {
 
 
     //print all the cities and their PM2.5 level
+	for(int i =0; i<cities.size(); i++) {
+		System.out.println(cities.get(i));
+	}
 
 
 
 
     //call the findMax method and print all the cities (using a loop) with the worst air quality
+	for(int i = 0; i < cities.size(); i++) {
+		if(cities.get(i).getPMValue() == findMax(cities)) {
+			System.out.println(cities.get(i).getName() + " has the worst air quality of " + cities.get(i).getPMValue());
+		}
+	}
 
 
 
 
     //call the findMin method and print all the cities (using a loop) with the best air quality
-
+	for(int i = 0; i < cities.size(); i++) {
+		if(cities.get(i).getPMValue() == findMin(cities)) {
+			System.out.println(cities.get(i).getName() + " has the best air quality of " + cities.get(i).getPMValue());
+		}
+	}
 
 
 
@@ -36,16 +49,26 @@ class Main {
 
 	// The method below should find the maximum pollution value in the ArrayList of City objects instantiated above 
 	// The method will need to call the getPMValue method in the City class to retrieve each city's pollution value and return the maxinum pollution value
-	public static int findMax(ArrayList<City> c) {
-
-		return -1;
+	public static int findMax(ArrayList<CityTwo> c) {
+		int temp = 0;
+		for(int i = 0; i < c.size(); i++){
+			if(c.get(i).getPMValue() > c.get(temp).getPMValue()) {
+				temp = i;
+			}
+		}
+		return c.get(temp).getPMValue();
 	}
 
   // The method below should find the minimum pollution value in the ArrayList of City objects instantiated above 
 	// The method will need to call the getPMValue method in the City class to retrieve each city's pollution value and return the minimum pollution value
-	public static int findMin(ArrayList<City> c) {
-
-		return -1;
+	public static int findMin(ArrayList<CityTwo> c) {
+		int temp = 0;
+		for(int i = 0; i < c.size(); i++){
+			if(c.get(i).getPMValue() < c.get(temp).getPMValue()) {
+				temp = i;
+			}
+		}
+		return c.get(temp).getPMValue();
 	}
 
  

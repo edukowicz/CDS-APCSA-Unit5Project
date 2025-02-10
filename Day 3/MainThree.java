f31ee086026941c40c571fb6aedb1e59eee06377
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-02-10 16:43:01 +0000

    message

--- Day 3/MainThree.java
+++ Day 3/MainThree.java
@@ -3,16 +3,19 @@ import java.util.ArrayList;
 
 //Use your assigned data set A,B,C, or D
 
-class Main {
+class MainThree {
 
   public static void main(String[] args) {
 
     //create an array for the city names
+    String[] cities = {"Columbo, Sri Lanka", "Manali, Chennai", "Secretariat, Amaravati", "Solapur, Solapur", "Katraj Pune", "MIDC Khutala, Chandraur"};
    
 
     //create 1D arrays for each of the rows of PM2.5 levels from the table - they must all be of the same data type 
-    
-
+    double[] columbo = {68, 30, 17, 46, 30, 42, 46, 70, 127, 95, 80, 65, 59, 53, 50, 53, 38, 59, 46, 55, 61, 65, 38, 53};
+    double[] manali = {67, 109, 125, 77, 89, 69, 112, 158, 124, 58, 86, 96, 76, 88, 87, 95, 99, 92, 116, 154, 157, 147, 104, 154};
+    double[] secretariat = {0, 0, 0, 63, 56, 28, 42, 33, 42, 0, 0, 24, 32, 38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    double[] solapur = {11, 3, 14, 9, 24, 35, 21, 21, 52, 58, 48, 50 , 58, 51, 37, 62, 80, 49, 17, 31, 49, 13, 34, 24};
 
 
 
